2bf6016f56d251d3c23b22390974b1e69caf9091
Guilherme Laux <guilherme.laux@compliancetotal.com.br>
2021-12-01 17:31:51 -0300

    adiciona css e remove arquivos

---
+++ src/components/Adress.tsx
@@ -0,0 +1,20 @@
+export default function Adress() {
+  return (
+    <>
+      <h1>ENDEREÇO</h1>
+
+      <div className='inputs-container'>
+        <input type='text' placeholder='CEP' />
+
+        <input type='text' placeholder='Rua' />
+
+        <input type='text' placeholder='Bairro' />
+
+        <input type='text' placeholder='Estado' />
+
+        <input type='text' placeholder='UF' />
+        
+      </div>
+    </>
+  )
+}
