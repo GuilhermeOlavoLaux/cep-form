6f8cc22b151aa300da7edab5ded0c67f2db65936
laux2laux@gmail.com <laux2laux@gmail.com>
2021-12-01 20:30:12 -0300

    adiciona api e logica para alterar os dados

--- src/components/Adress.tsx
+++ src/components/Adress.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent, useState } from 'react'
+import cepRoute from '../api/routes'
 
 interface IFields {
   cep: string
-  state: string
   uf: string
   city: string
   street: string
@@ -10,32 +10,44 @@ interface IFields {
 }
 
 export default function Adress() {
-
   const [fields, setFields] = useState({} as IFields)
+
+  async function setFieldsByCep(cep: string) {
+    const cepResponse = await cepRoute.get(`/${cep}/json/`)
+
+    if (cepResponse.data) {
+      setFields({
+        ...fields,
+        uf: cepResponse.data.uf,
+        city: cepResponse.data.localidade,
+        street: cepResponse.data.logradouro,
+        neighborhood: cepResponse.data.bairro
+      })
+    } else {
+      return
+    }
+  }
 
   return (
     <>
-      <h1>ENDEREÇO</h1>
-
+      <h1>Endereço</h1>
       <div className='inputs-container'>
         <input
           type='text'
           placeholder='CEP'
+          value={fields.cep}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, cep: event.target.value })
           }
+          onBlur={() => {
+            setFieldsByCep(fields.cep)
+          }}
         />
 
         <input
           type='text'
-          placeholder='Estado'
-          onChange={(event: ChangeEvent<HTMLInputElement>) =>
-            setFields({ ...fields, state: event.target.value })
-          }
-        />
-        <input
-          type='text'
           placeholder='UF'
+          value={fields.uf}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, uf: event.target.value })
           }
@@ -44,6 +56,7 @@ export default function Adress() {
         <input
           type='text'
           placeholder='Cidade'
+          value={fields.city}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, city: event.target.value })
           }
@@ -52,6 +65,7 @@ export default function Adress() {
         <input
           type='text'
           placeholder='Rua'
+          value={fields.street}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, street: event.target.value })
           }
@@ -60,6 +74,7 @@ export default function Adress() {
         <input
           type='text'
           placeholder='Bairro'
+          value={fields.neighborhood}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, neighborhood: event.target.value })
           }
