8ce40d371b6d15bec65f7f9295311ce1a529a7f9
laux2laux@gmail.com <laux2laux@gmail.com>
2021-12-01 19:16:30 -0300

    cria interface e estados

--- src/components/Adress.tsx
+++ src/components/Adress.tsx
@@ -1,19 +1,69 @@
+import { ChangeEvent, useState } from 'react'
+
+interface IFields {
+  cep: string
+  state: string
+  uf: string
+  city: string
+  street: string
+  neighborhood: string
+}
+
 export default function Adress() {
+
+  const [fields, setFields] = useState({} as IFields)
+
   return (
     <>
       <h1>ENDEREÇO</h1>
 
       <div className='inputs-container'>
-        <input type='text' placeholder='CEP' />
+        <input
+          type='text'
+          placeholder='CEP'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, cep: event.target.value })
+          }
+        />
 
-        <input type='text' placeholder='Rua' />
+        <input
+          type='text'
+          placeholder='Estado'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, state: event.target.value })
+          }
+        />
+        <input
+          type='text'
+          placeholder='UF'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, uf: event.target.value })
+          }
+        />
 
-        <input type='text' placeholder='Bairro' />
+        <input
+          type='text'
+          placeholder='Cidade'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, city: event.target.value })
+          }
+        />
 
-        <input type='text' placeholder='Estado' />
+        <input
+          type='text'
+          placeholder='Rua'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, street: event.target.value })
+          }
+        />
 
-        <input type='text' placeholder='UF' />
-        
+        <input
+          type='text'
+          placeholder='Bairro'
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setFields({ ...fields, neighborhood: event.target.value })
+          }
+        />
       </div>
     </>
   )
