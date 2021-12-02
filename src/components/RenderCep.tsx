d6d472392f0abc4297db0181bb15ffe4e63b7ece
laux2laux@gmail.com <laux2laux@gmail.com>
2021-12-01 23:16:54 -0300

    remove console log e seta o bairro

--- src/components/RenderCep.tsx
+++ src/components/RenderCep.tsx
@@ -13,20 +13,19 @@ export default function RenderCep() {
   const [fields, setFields] = useState({} as IFields)
 
   async function setFieldsByCep(uf: string, city: string, street: string) {
-    console.log(`https://viacep.com.br/ws/${uf}/${city}/${street}/json/`)
     const cepResponse = await api.get(`${uf}/${city}/${street}/json/`)
 
     if (cepResponse.data.length > 0) {
       setFields({
         ...fields,
-        cep: cepResponse.data[0].cep
+        cep: cepResponse.data[0].cep,
+        neighborhood: cepResponse.data[0].bairro
       })
     }
   }
 
   return (
     <>
-      {console.log(fields)}
       <h1>RenderCep</h1>
       <div className='inputs-container'>
         <input
@@ -41,6 +40,7 @@ export default function RenderCep() {
         <input
           type='text'
           placeholder='UF'
+          value={fields.uf}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, uf: event.target.value })
           }
@@ -52,6 +52,7 @@ export default function RenderCep() {
         <input
           type='text'
           placeholder='Cidade'
+          value={fields.city}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, city: event.target.value })
           }
@@ -63,6 +64,7 @@ export default function RenderCep() {
         <input
           type='text'
           placeholder='Rua'
+          value={fields.street}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, street: event.target.value })
           }
@@ -74,6 +76,7 @@ export default function RenderCep() {
         <input
           type='text'
           placeholder='Bairro'
+          value={fields.neighborhood}
           onChange={(event: ChangeEvent<HTMLInputElement>) =>
             setFields({ ...fields, neighborhood: event.target.value })
           }
